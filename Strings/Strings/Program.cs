a3ca907a2960a38da457a622079e4575a69b42a1
HeyaIe <bryngx@gmail.com>
2022-03-16 22:16:29 -0400

    Added folder "Strings"

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string message = " is turning ";
+            string name = "Heyale";
+            int turningAge = 7;
+
+            Console.WriteLine(name.ToUpper() + message + turningAge.ToString() + ".");
+
+            // Stringbuilder
+            StringBuilder str = new StringBuilder();
+            str.Append("My name is Bryan");
+            str.Append(", I am turning");
+            str.Append(" 28.");
+
+            Console.WriteLine(str);
+
+
+            Console.ReadLine();
+        }
+    }
+}
