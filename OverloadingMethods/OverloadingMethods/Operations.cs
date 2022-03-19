9f4c9909359e85459aa330c46c7a9e2f616dcd74
HeyaIe <bryngx@gmail.com>
2022-03-19 18:16:02 -0400

    Added folder "OverloadingMethods"

---
+++ OverloadingMethods/OverloadingMethods/Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethods
+{
+    public class Operations
+    {
+        public int operation(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public decimal operation(decimal num1, decimal num2)
+        {
+            return num1 / num2;
+        }
+        public int operation(string num1, string num2)
+        {
+            int numb1 = Convert.ToInt32(num1);
+            int numb2 = Convert.ToInt32(num2);
+
+            return numb1 + numb2;
+        }
+    }
+}
---
+++ OverloadingMethods/OverloadingMethods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Introduction
+            Console.WriteLine("Overloading Methods");
+            Console.WriteLine("--------------------------------------------");
+
+            // Create an object of class
+            Operations a = new Operations();
+
+            // Prompt user for two numbers
+            Console.WriteLine("Please enter a number:");
+            string input1 = Console.ReadLine();
+            int num1 = Convert.ToInt32(input1);
+            Console.WriteLine("Please enter a second number:");
+            string input2 = Console.ReadLine();
+            int num2 = Convert.ToInt32(input2);
+
+            // Display result
+            Console.WriteLine("{0} * {1} = " + a.operation(num1, num2), num1, num2);
+
+            // Overloading operation method with decimal arguments
+            Console.WriteLine("15.2 / 13.1 = " + a.operation(15.2m, 13.1m));
+
+            // Overloading operation method with string arguments
+            Console.WriteLine("105 + 206 = " + a.operation("105", "206"));
+
+            Console.Read();
+        }
+    }
+}
