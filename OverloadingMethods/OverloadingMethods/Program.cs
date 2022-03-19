abb2abd93f12945dd81c6dde9f46635ae1fad137
HeyaIe <bryngx@gmail.com>
2022-03-19 18:51:29 -0400

    Code edit, no user input

--- OverloadingMethods/OverloadingMethods/Program.cs
+++ OverloadingMethods/OverloadingMethods/Program.cs
@@ -17,16 +17,9 @@ namespace OverloadingMethods
             // Create an object of class
             Operations a = new Operations();
 
-            // Prompt user for two numbers
-            Console.WriteLine("Please enter a number:");
-            string input1 = Console.ReadLine();
-            int num1 = Convert.ToInt32(input1);
-            Console.WriteLine("Please enter a second number:");
-            string input2 = Console.ReadLine();
-            int num2 = Convert.ToInt32(input2);
 
             // Display result
-            Console.WriteLine("{0} * {1} = " + a.operation(num1, num2), num1, num2);
+            Console.WriteLine("10 * 3 = " + a.operation(10, 3));
 
             // Overloading operation method with decimal arguments
             Console.WriteLine("15.2 / 13.1 = " + a.operation(15.2m, 13.1m));
