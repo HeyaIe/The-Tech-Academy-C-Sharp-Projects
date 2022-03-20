9612beca370f5350ab5c5c963fddf19d79952c95
HeyaIe <bryngx@gmail.com>
2022-03-20 18:38:31 -0400

    Added folder "OutParam"

---
+++ OutParam/OutParam/Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutParam
+{
+    class Operation
+    {
+        public void operation1(out int num1, out int num2)
+        {
+            num1 = 15;
+            num2 = num1 - 5;
+            Console.WriteLine("Result: " + num2);
+        }
+    }
+}
---
+++ OutParam/OutParam/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutParam
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Introduction
+            Console.WriteLine("Out Parameters");
+
+            // Insantiate object from class
+            Operation a = new Operation();
+
+            // Initalize two variables
+            int num1 = 0;
+            int num2 = 0;
+
+            // Call method passing in two out parameters
+            a.operation1(out num1, out num2);
+
+            Console.Read();
+        }
+    }
+}
