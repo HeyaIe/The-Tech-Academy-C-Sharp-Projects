051769f10781983102ce5b7308e6a19139bcfc06
HeyaIe <bryngx@gmail.com>
2022-03-21 19:00:53 -0400

    Added folder "OverloadingOperator"

---
+++ OverloadingOperator/OverloadingOperator/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperator
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public static bool operator==(Employee employee1, Employee employee2)
+        {
+            if(employee1.Id == employee2.Id)
+            {
+                Console.WriteLine("{0} and {1} are the same person.", employee1.firstName, employee2.firstName);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("{0} and {1} are not the same person.", employee1.firstName, employee2.firstName);
+                return false;
+            }
+        }
+        public static bool operator!=(Employee employee1, Employee employee2)
+        {
+            if(employee1.Id != employee2.Id)
+            {
+                Console.WriteLine("{0} and {1} are not the same person.", employee1.firstName, employee2.firstName);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ OverloadingOperator/OverloadingOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperator
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Introduction
+            Console.WriteLine("Overloading Operator\n");
+
+            // Instantiating two employee objects
+            Employee person1 = new Employee() { firstName = "Bryan", Id = 13 };
+            Employee person2 = new Employee() { firstName = "Sam", Id = 1 };
+
+            // Calling overloaded == operator
+            Console.WriteLine(person1 == person2);
+
+            Console.Read();
+        }
+    }
+}
