6abde7320d74326a08d003606cd31021dc937d95
HeyaIe <bryngx@gmail.com>
2022-03-21 11:30:54 -0400

    Added folder "MethodsAndObjects"

---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Introduction
+            Console.WriteLine("Class Inheritance\n");
+
+            // Instantiate + initalize employee object
+            Employee emp = new Employee() { FirstName = "Sample", LastName = "Student" };
+            // Call super method
+            emp.SayName();
+
+            Console.Read();
+        }
+    }
+}
