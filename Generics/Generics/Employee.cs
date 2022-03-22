b65cf2d1498e8aefdec0be4db97c45741614950b
HeyaIe <bryngx@gmail.com>
2022-03-22 11:48:34 -0400

    Added folder "Generics"

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Employee<T> : Person
+    {
+        public int Id { get; set; }
+        public List<T> things { get; set; }
+
+        public void listItems()
+        {
+            foreach(var i in things)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Introduction
+            Console.WriteLine("Generics");
+
+            // Instantiate employee object
+            Employee<string> em1 = new Employee<string>();
+            em1.things = new List<string>() { "screws", "powerdrill", "workbench", "tracksaw", "marker" };
+
+            Employee<int> em2 = new Employee<int>();
+            em2.things = new List<int>() { 1, 10, 23, 21, 16, 4 };
+
+            // Display list
+            Console.WriteLine("\nEmployee 1");
+            Console.WriteLine("-------------------------------------------");
+            em1.listItems();
+
+            Console.WriteLine("\nEmployee 2");
+            Console.WriteLine("-------------------------------------------");
+            em2.listItems();
+
+            Console.Read();
+        }
+    }
+}
