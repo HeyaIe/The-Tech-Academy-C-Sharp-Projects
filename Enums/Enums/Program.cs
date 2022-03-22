8fc8fa7c39642e996104659ac208d8e78deb0d7a
HeyaIe <bryngx@gmail.com>
2022-03-22 14:11:02 -0400

    Added folder "Enums"

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Introduction
+            Console.WriteLine("Enums");
+
+            // Prompt user to enter current day
+            Console.WriteLine("\nPlease enter the current day of the week.");
+
+            try
+            {
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+                Console.WriteLine(day);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+        }
+    }
+}
