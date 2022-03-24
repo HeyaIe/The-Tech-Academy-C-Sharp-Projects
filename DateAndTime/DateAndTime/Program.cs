c33d819eb1b0f3ade61cbba47b0393ee8f5a220e
HeyaIe <bryngx@gmail.com>
2022-03-23 21:44:04 -0400

    Added folder "DateAndTime"

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Introduction
+            Console.WriteLine("DateTime");
+            // Display current date time.
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine(currentDateTime);
+
+            // Prompt user for a number
+            Console.WriteLine("Please enter a number:");
+
+            // Display updated date time after adding hours
+            Console.WriteLine(DateTime.Now.AddHours(Convert.ToDouble(Console.ReadLine())));
+
+            Console.Read();
+        }
+    }
+}
